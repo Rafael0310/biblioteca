499ba2d216081772643eb1cfa5d09dc400158947
Rafael Abreu <rafael.abreu031015@gmail.com>
2024-06-22 18:20:19 -0300

    update all files

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
-from livro import (
-    Livro,
-    adicionar_livro
-)
+# Importação de bibliotecas
+import os
+
+# Importação de classes
+from livro import Livro
+from membro import Membro
+
+while True:
+    print('''____________________________________\n
+       Sistema bibliotecário
+____________________________________\n
+          Menu Principal
+____________________________________\n
+      1 - Adicionar membro
+      2 - Adicionar livro
+      3 - Emprestar livro
+      4 - Devolver livro
+      5 - Sair\n
+____________________________________\n''')
+    
+    try:
+        opcao = int(input('O que deseja?'))
+
+        match opcao:
+            case 1:
+                Membro.adicionar_membro()
+                os.system('cls')
+                pass
+            case 2:
+                Livro.adicionar_livro()
+                os.system('cls')
+                pass
+            case 3:
+                os.system('cls')
+                pass
+            case 4:
+                os.system('cls')
+                pass
+            case 5:
+                os.system('cls')
+                print('Encerrando...')
+                quit()
+            case _:
+                os.system('cls')
+                print('Opção inválida! Tente novamente.')
+        
+    except  ValueError:
+        print('Por favor, insira um número inteiro.')
--- membro.py
+++ membro.py
@@ -1,18 +1,36 @@
 from dataclasses import dataclass
+from livro import (
+    Livro,
+    lista_livros
+)
 
-lista_membros = []
+lista_membros = {}
+livro_encontrado = False
 
 @dataclass
 class Membro:
     id_membro: int
     nome: str
-    livros_emprestados = []
 
-def adicionar_membro():
-    id_membro = input('Informe o ID do membro:\n')
-    nome = input('Informe o nome do membro:\n')
-    lista_membros.append(Membro(id_membro=id_membro, nome=nome))
-    print(f'Membro adicionado com sucesso!\nID: {id_membro}\nNome: {nome}')
+    ids_membros = set()
 
+    # Função para adicionar um novo membro
+    def adicionar_membro():
+        id_membro = input('Informe o ID do membro:\n')
+        nome = input('Informe o nome do membro:\n')
+        lista_membros.append(Membro(id_membro=id_membro, nome=nome))
+        print(f'Membro adicionado com sucesso!\nID: {id_membro}\nNome: {nome}')
 
-adicionar_membro()
+    # Função para emprestar livro
+    def emprestar_livro():
+        livro_interessado = input('Qual livro será emprestado? ').strip
+        for livro in lista_livros.values():
+            if livro == livro_interessado:
+                livro_encontrado = True
+                break
+        
+        #if livro_encontrado:
+        #    lista_membros.append(livro_interessado)
+        #    print(f'O livro: {livro_interessado} foi emprestado\n')
+        #else:
+        #    print('O livro não foi localizado')
